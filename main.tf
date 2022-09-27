f57f16d242cfe172aa9fb0bae5995c67828f670c
Eric Claver AKAFFOU <akaffou.eric@me.com>
2022-09-27 22:57:15 +0200

    Update

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "kubernetes" {
-  # load_config_file       = false
   cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
   host                   = var.kubernetes_cluster_endpoint
   exec {
@@ -11,7 +10,6 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    #load_config_file       = false
     cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
     host                   = var.kubernetes_cluster_endpoint
     exec {
@@ -23,12 +21,36 @@ provider "helm" {
 }
 
 
-# provider "aws" {
-#   region = var.aws_region
-# }
-# data "aws_eks_cluster" "msur" {
-#   name = var.kubernetes_cluster_id
-# }
+/*
+provider "aws" {
+  region = var.aws_region
+}
+data "aws_eks_cluster" "msur" {
+  name = var.kubernetes_cluster_id
+}
+provider "kubernetes" {
+  load_config_file       = false
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
+  host                   = data.aws_eks_cluster.msur.endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws-iam-authenticator"
+    args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
+  }
+}
+provider "helm" {
+  kubernetes {
+    load_config_file       = false
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.msur.certificate_authority.0.data)
+    host                   = data.aws_eks_cluster.msur.endpoint
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws-iam-authenticator"
+      args        = ["token", "-i", "${data.aws_eks_cluster.msur.name}"]
+    }
+  }
+}
+*/
 
 resource "kubernetes_namespace" "argo-ns" {
   metadata {
